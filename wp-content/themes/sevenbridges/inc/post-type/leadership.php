a00a7a05d05c651b6722b1decbcd09172ce266c9
goldengatetechnolabs <goldengatetechnolabs@gmail.com>
2022-10-21 17:46:22 +0530

    21/10/2022 update

--- wp-content/themes/sevenbridges/inc/post-type/leadership.php
+++ wp-content/themes/sevenbridges/inc/post-type/leadership.php
@@ -1,44 +1,58 @@
-<?php
-function leadership_custom_post_type() {
-	$args = [
-		'label'  => esc_html__( 'Leaderships', 'text-domain' ),
-		'labels' => [
-			'menu_name'          => esc_html__( 'Leaderships', 'sevenBridges' ),
-			'name_admin_bar'     => esc_html__( 'Leadership', 'sevenBridges' ),
-			'add_new'            => esc_html__( 'Add Leadership', 'sevenBridges' ),
-			'add_new_item'       => esc_html__( 'Add new Leadership', 'sevenBridges' ),
-			'new_item'           => esc_html__( 'New Leadership', 'sevenBridges' ),
-			'edit_item'          => esc_html__( 'Edit Leadership', 'sevenBridges' ),
-			'view_item'          => esc_html__( 'View Leadership', 'sevenBridges' ),
-			'update_item'        => esc_html__( 'View Leadership', 'sevenBridges' ),
-			'all_items'          => esc_html__( 'All Leaderships', 'sevenBridges' ),
-			'search_items'       => esc_html__( 'Search Leaderships', 'sevenBridges' ),
-			'parent_item_colon'  => esc_html__( 'Parent Leadership', 'sevenBridges' ),
-			'not_found'          => esc_html__( 'No Leaderships found', 'sevenBridges' ),
-			'not_found_in_trash' => esc_html__( 'No Leaderships found in Trash', 'sevenBridges' ),
-			'name'               => esc_html__( 'Leaderships', 'sevenBridges' ),
-			'singular_name'      => esc_html__( 'Leadership', 'sevenBridges' ),
-		],
-		'public'              => true,
-		'exclude_from_search' => false,
-		'publicly_queryable'  => true,
-		'show_ui'             => true,
-		'show_in_nav_menus'   => true,
-		'show_in_admin_bar'   => true,
-		'show_in_rest'        => true,
-		'capability_type'     => 'post',
-		'hierarchical'        => false,
-		'has_archive'         => true,
-		'query_var'           => true,
-		'can_export'          => true,
-		'rewrite_no_front'    => false,
-		'show_in_menu'        => true,
-		'menu_icon'           => 'dashicons-businessman',
-		'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'author', 'comments', 'trackbacks', 'page-attributes', 'post-formats', 'custom-fields'),
-		
-		'rewrite' => true
-	];
+<?php 
+// Register Leadership Post Type
+function leadership_post_type() {
 
+	$labels = array(
+		'name'                  => _x( 'Leadership', 'Post Type General Name', 'sevenBridges' ),
+		'singular_name'         => _x( 'Leadership', 'Post Type Singular Name', 'sevenBridges' ),
+		'menu_name'             => __( 'Leadership', 'sevenBridges' ),
+		'name_admin_bar'        => __( 'Leadership', 'sevenBridges' ),
+		'archives'              => __( 'Item Archives', 'sevenBridges' ),
+		'attributes'            => __( 'Item Attributes', 'sevenBridges' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'sevenBridges' ),
+		'all_items'             => __( 'All Items', 'sevenBridges' ),
+		'add_new_item'          => __( 'Add New Item', 'sevenBridges' ),
+		'add_new'               => __( 'Add New', 'sevenBridges' ),
+		'new_item'              => __( 'New Item', 'sevenBridges' ),
+		'edit_item'             => __( 'Edit Item', 'sevenBridges' ),
+		'update_item'           => __( 'Update Item', 'sevenBridges' ),
+		'view_item'             => __( 'View Item', 'sevenBridges' ),
+		'view_items'            => __( 'View Items', 'sevenBridges' ),
+		'search_items'          => __( 'Search Item', 'sevenBridges' ),
+		'not_found'             => __( 'Not found', 'sevenBridges' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'sevenBridges' ),
+		'featured_image'        => __( 'Featured Image', 'sevenBridges' ),
+		'set_featured_image'    => __( 'Set featured image', 'sevenBridges' ),
+		'remove_featured_image' => __( 'Remove featured image', 'sevenBridges' ),
+		'use_featured_image'    => __( 'Use as featured image', 'sevenBridges' ),
+		'insert_into_item'      => __( 'Insert into item', 'sevenBridges' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'sevenBridges' ),
+		'items_list'            => __( 'Items list', 'sevenBridges' ),
+		'items_list_navigation' => __( 'Items list navigation', 'sevenBridges' ),
+		'filter_items_list'     => __( 'Filter items list', 'sevenBridges' ),
+	);
+	$args = array(
+		'label'                 => __( 'Leadership', 'sevenBridges' ),
+		'description'           => __( 'Leadership Description', 'sevenBridges' ),
+		'labels'                => $labels,
+		'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'author', 'comments', 'trackbacks', 'page-attributes', 'post-formats', 'custom-fields'),
+		'taxonomies'            => array( ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_icon'   => 'dashicons-businessman',
+		'menu_position'         => 3,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
 	register_post_type( 'leadership', $args );
+
 }
-add_action( 'init', 'leadership_custom_post_type' );
+add_action( 'init', 'leadership_post_type', 0 );
+
